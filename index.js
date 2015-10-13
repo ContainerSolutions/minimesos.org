ea8f970722eebe1f7e50cc96fc589657fe9c8467
Aleksandr Guljajev <alexandr.gulyayev@gmail.com>
2015-10-13 16:45:53 +0200

    Adding version reader; front-end fixes

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 // Bootstrap wants jQuery global =(
 window.jQuery = $ = require('jquery')
-
-var bootstrap = require('bootstrap/dist/js/bootstrap')
+var fs = require('fs')
+var bootstrap = require('./node_modules/bootstrap/dist/js/bootstrap')
 
 // Get Bootstrap styles with cssify
 var bootstrapStyle = require('./node_modules/bootstrap/dist/css/bootstrap.css')
 var customStyle = require('./site/assets/css/main.css')
+
+var ver = fs.readFileSync('./site/version', 'utf-8')
+
+$(function () {
+    $(".download-area > span").html("Latest version: " + ver)
+})
