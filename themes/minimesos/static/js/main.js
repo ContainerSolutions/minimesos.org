e79dbd19d3c25286c1d2f8f86a93e5f597ca87a2
Aleksandr Guljajev <gulyayev.alex@gmail.com>
2016-02-16 12:32:33 +0100

    Replacing with hugo

--- themes/minimesos/static/js/main.js
+++ themes/minimesos/static/js/main.js
@@ -8,7 +8,7 @@ var bootstrap = require('./node_modules/bootstrap/dist/js/bootstrap')
 var bootstrapStyle = require('./node_modules/bootstrap/dist/css/bootstrap.css')
 var customStyle = require('./site/assets/css/main.css')
 
-var ver = "0.4.0"
+var ver = "0.5.0"
 
 $(function () {
     $(".download-area > span").html("Latest version: " + ver)
